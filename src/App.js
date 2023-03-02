a50ab78063b3ac111312370953e842f7165a744f
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-03-03 01:33:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Todo from "./use_reducer/Todo";
+
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/use_reducer/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Modal = (props) => {
+  return (
+    <div>
+        <h4>
+            {props.modal}
+        </h4>
+    </div>
+  )
+}
+
+export default Modal
